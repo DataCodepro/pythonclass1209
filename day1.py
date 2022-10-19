a47a49839cdb7949603c62a7406dd846a28f8080
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2022-10-19 10:59:07 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,50 @@
+print('Hello World')
+#variables
+name = 'Taiwo'
+number1 = 45
+number2 = 12.6
+X = True
+print(name)
+# have a variable holding your firstname
+# have a variable holding any integer value
+# have a variable holding any float value
+# have a variable holding any of the boolean value
+# display all the variables you have declearded to  return their out
+
+#Escape character
+word = 'we\'re brothers from the other side of the town'
+print(word)
+word2 = "we're brothers from the other side of the town"
+#New line
+word3 = 'python is fun\npython is easy\npython is interactive in nature'
+print(word3)
+
+
+#string concatenation which means joining of two or more string
+print('hello' + ' '+ 'world')
+print('welcome to python class' +' '+name)
+surname = 'Kanjuni'
+print(surname + ' '+ name)
+
+# String formatting which means formatting a value of a variable inside a string
+price1 = 15000
+price2 = 30500
+price3 = 45000
+sales_report = 'I sold two pairs of shirt for {}, two pairs of shoe for {} and a suit for {}'
+print(sales_report.format(price1,price2,price3))
+print(f'I sold two pairs of shirt for {price1}, two pairs of shoe for {price2} and a suit for {price3}')
+
+
+#String Method this  are function that can only be applied on a string
+word1 = 'python'
+word2 = 'PYTHON'
+word3 = 'python is fun'
+word4 = '   info'
+print(word1.upper())
+print(word2.lower())
+ 0print(word3.title())
+print(word3.capitalize())
+print(word3.split())
+print(word4.strip())
+
+
